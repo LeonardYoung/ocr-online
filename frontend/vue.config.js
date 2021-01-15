52d9251791b1a2837f49726e83a378ef1fc3c7e1
LeonardYoung <shaojun47@qq.com>
2021-01-15 15:19:02 +0800

    backup before split OCR and tableOCR into two Page

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-06 15:50:28
- * @LastEditTime: 2020-12-19 16:16:14
+ * @LastEditTime: 2021-01-15 11:27:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ocrs\frontend\vue.config.js
@@ -11,8 +11,8 @@ module.exports = {
         proxy:{
             '/api':{
                 // target: 'http://127.0.0.1:5000/' ,
-                // target: 'http://172.17.169.235:32776/' ,
-                target: 'http://172.17.171.8:5000/' ,
+                target: 'http://172.17.169.235:5000/' ,
+                // target: 'http://172.17.171.8:5000/' ,
                 changeOrigin:true,
                 pathRewrite: {
                     '^/api': '/api'
